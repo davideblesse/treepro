bfb7510139715d763aaace8322551348216239b2
dnlsdn <danielesadun@gmail.com>
2025-05-18 14:20:59 +0200

    Bump version to 0.1.1 and include package data

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="treepro",
-    version="0.1.0",
+    version="0.1.1",
     description="An advanced version of the Unix tree command.",
     author="Your Name",
     author_email="your.email@example.com",
